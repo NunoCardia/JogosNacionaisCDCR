03bc98db63e414e4801a6d1678dc9fc874a2d320
Nuno Ferreira <nunocardia_21@hotmail.com>
2018-05-28 15:23:57 +0100

    Trying to fix mobile version

--- js/myscript.js
+++ js/myscript.js
@@ -12,57 +12,29 @@ $(document).ready(function(){
         $(".navbar").css('margin','0 auto');
         $(".navbar").css('text-align','center');
     }
-    /*if(start_change.length){
+    if(start_change.length){
         $(document).scroll(function(){
             scroll_start = $(this).scrollTop();
             if(scroll_start > offset.top){
                 $(".navbar").css('background-color','#808080');
+                $(".navbar").css('height','50px !important');
                 $(".navbar-brand").css('display','none');
-                $(".navbar").css('height','70px');
-                $(".navbar").css('transition','all 1s');
-                $(".navbar-nav > a").css('color','white');
-                $(".dropdown > a").css('color','white');
-                $("#myTogglerNav").css('color','white');
-                $(".navbar-nav > a").hover(function(){
-                    $(this).css("color", "white");
-                    }, function(){
-                    $(this).css("color", "white");
-                });
-                $(".dropdown > a").hover(function(){
-                    $(this).css("color", "white");
-                    }, function(){
-                    $(this).css("color", "white");
-            });
-                $(".navbar-nav > a").hover(function(){
-                    $(this).css("border-bottom", "2px solid white");
-                    }, function(){
-                    $(this).css("border-bottom", "");
-            });
-                
+                $(".navbar-nav > a").css('color','#2d2d2d');
+                $(".dropdown > a").css('color','#2d2d2d');
+                $("#myTogglerNav").css('color','black');
             }
             else{
                 $(".navbar").css('background-color','white');
                 $(".navbar-nav > a").css('color','#2d2d2d');
                 $(".dropdown > a").css('color','#2d2d2d');
-                $(".navbar-brand").css('display','block');
-                $(".navbar").css('height','98px');
+                $(".navbar-brand").css('display','inline-block');
                 $(".navbar-nav > a").hover(function(){
-                    $(this).css("color", "#ee3733");
-                    }, function(){
-                    $(this).css("color", "#2d2d2d");
-            });
-                $(".dropdown > a").hover(function(){
-                    $(this).css("color", "#ee3733");
-                    }, function(){
-                    $(this).css("color", "#2d2d2d");
-            });
-                $(".navbar-nav > a").hover(function(){
-                    $(this).css("border-bottom", "2px solid #ee3733");
+                    $(this).css("border-bottom", "2px solid white");
                     }, function(){
                     $(this).css("border-bottom", "");
             });
             }
         });
-    }*/
+    }
 });
 
